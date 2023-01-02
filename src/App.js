bd4aed482ccd6499e7f979d9f68acb58858246a6
NirmalGundlapelly <nirmalgoud2103@gmail.com>
2023-01-02 06:01:01 +0000

    v1

---
+++ src/App.js
@@ -0,0 +1,8 @@
+// eslint-disable-next-line import/no-named-as-default-member
+import Capitals from './components/Capitals'
+
+import './App.css'
+
+const App = () => <Capitals />
+
+export default App
